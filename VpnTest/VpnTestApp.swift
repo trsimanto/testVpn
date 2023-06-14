cc2217e6f9a540e78876b2480c39fa710fc1dc51
trsimanto <simanto14feb@gmail.com>
2023-06-15 01:44:36 +0600

    init

---
+++ VpnTest/VpnTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  VpnTestApp.swift
+//  VpnTest
+//
+//  Created by REEA on 14/6/23.
+//
+
+import SwiftUI
+
+@main
+struct VpnTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ test/AppProxyProvider.swift
@@ -0,0 +1,12 @@
+//
+//  AppProxyProvider.swift
+//  test
+//
+//  Created by REEA on 15/6/23.
+//
+
+import NetworkExtension
+import TunnelKitOpenVPNAppExtension
+
+class AppProxyProvider: OpenVPNTunnelProvider {
+}
